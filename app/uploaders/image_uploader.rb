a45d902c1cc3a152c32418067b86063f1b38f6b7
mattstuddert <mstuddert@nuveer.com>
2014-06-19 16:23:50 +0100

    added image into assets

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -24,6 +24,34 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
+  version :player_image do 
+      process :resize_to_fill => [400,600]
+  end
+ 
+ 
+  # version :facepalm do
+  #   process :create_composite
+  # end
+ 
+  # def create_composite
+  #   manipulate! do |img|
+
+  #     facepalm_path =  File.join(Rails.root, "app/assets/images/facepalm.png")
+  #     facepalm = Magick::Image.read(facepalm_path).first         
+
+ 
+  #     # facepalm.resize_to_fill(150,220)
+
+ 
+ 
+  #     img.composite!(money, 0, 0, Magick::OverCompositeOp)
+  #   end
+  # end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
   # Process files as they are uploaded:
   # process :scale => [200, 300]
   #
@@ -38,9 +66,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
